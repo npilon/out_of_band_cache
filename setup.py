371d52a79f25e3d524d3e4fc0b653d5ff4976968
Nick Pilon <npilon@oreilly.com>
2009-07-10 16:38:48 -0700

    Initial project creation.

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='out_of_band_cache',
+      version=version,
+      description="A beaker cache extension that supports out of band updates.",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Nick Pilon',
+      author_email='npilon@oreilly.com',
+      url='',
+      license='',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+          'beaker',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
