59ced1b0dae3182f3326133a76fbf846991980e0
Nick Pilon <npilon@oreilly.com>
2009-07-15 15:31:56 -0700

    Switch to queue-based updates.

--- out_of_band_cache/__init__.py
+++ out_of_band_cache/__init__.py
@@ -8,6 +8,9 @@ inherently thread-safe, this is very convenient."""
 
 import threading
 import logging
+from Queue import Queue, Empty
+import sys
+import traceback
 
 import beaker.cache
 import beaker.middleware
@@ -19,6 +22,28 @@ if logger.isEnabledFor(logging.DEBUG):
 else:
     def debug(message, *args):
         pass
+error = logger.error
+
+class SingleEntryQueue(Queue):
+    """Queue implementation that only allows for one item to be in the Queue at a time"""
+    
+    def _put(self, item):
+        if item not in self.queue:
+            self.queue.append(item)
+
+            
+class Update(object):
+    def __init__(self, update_for, callback):
+        self.update_for = update_for
+        self.callback = callback
+    def __hash__(self):
+        return hash(self.update_for)
+    def __eq__(self, other):
+        return self.update_for == other
+    def __repr__(self):
+        return "<%s.%s instance update for: %r>" % (self.__class__.__module__,
+                                                    self.__class__.__name__,
+                                                    self.update_for)
 
 class CacheMiddleware(beaker.middleware.CacheMiddleware):
     def __init__(self, *args, **kwargs):
@@ -30,13 +55,17 @@ class CacheManager(beaker.cache.CacheManager):
         self.kwargs = beaker_cache_manager.kwargs
         self.caches = beaker_cache_manager.caches
         self.regions = beaker_cache_manager.regions
+        self.queue = SingleEntryQueue()
+        update_thread = threading.Thread(target=update_processor,
+                                         kwargs={'queue': self.queue,},)
+        update_thread.start()
     
     def get_cache(self, name, out_of_band=False, **kwargs):
         if not out_of_band:
             return super(CacheManager, self).get_cache(name, **kwargs)
         kw = self.kwargs.copy()
         kw.update(kwargs)
-        return self.caches.setdefault(name + str(kw), Cache(name, **kw))
+        return self.caches.setdefault(name + str(kw), Cache(name, self.queue, **kw))
     
     def get_cache_region(self, name, region, out_of_band=False):
         if not out_of_band:
@@ -44,9 +73,13 @@ class CacheManager(beaker.cache.CacheManager):
         if region not in self.regions:
             raise BeakerException('Cache region not configured: %s' % region)
         kw = self.regions[region]
-        return self.caches.setdefault(name + str(kw), Cache(name, **kw))
+        return self.caches.setdefault(name + str(kw), Cache(name, self.queue, **kw))
 
 class Cache(beaker.cache.Cache):
+    def __init__(self, name, queue, **kw):
+        super(Cache, self).__init__(name, **kw)
+        self.queue = queue
+    
     def _get_value(self, key, **kw):
         if isinstance(key, unicode):
             key = key.encode('ascii', 'backslashreplace')
@@ -57,7 +90,7 @@ class Cache(beaker.cache.Cache):
         kw.setdefault('expiretime', self.expiretime)
         kw.setdefault('starttime', self.starttime)
         
-        return container.Value(key, self.namespace, **kw)
+        return Value(key, self.namespace, self.queue, **kw)
 
 class NewValueInProgressException(Exception):
     """Raised when a request is made for a value we don't have in the cache."""
@@ -65,6 +98,9 @@ class NewValueInProgressException(Exception):
 
 class Value(beaker.container.Value):
     """A Value that will still return - but allows querying on - expired keys."""
+    def __init__(self, key, namespace, queue, **kw):
+        super(Value, self).__init__(key, namespace, **kw)
+        self.queue = queue
     
     def get_value(self):
         # Attempt to read the value out of the store.
@@ -85,53 +121,39 @@ class Value(beaker.container.Value):
         finally:
             self.namespace.release_read_lock()
         
-        # No value or expired value; attempt to get the create lock.
-        has_createlock = False
-        creation_lock = self.namespace.get_creation_lock(self.key)
-        if has_value:
-            # This branch will either return immediately or get the lock.
-            if not creation_lock.acquire(wait=False):
-                debug("get_value returning old value while new one is created")
-                return value
-            else:
-                debug("lock_creatfunc (didnt wait)")
-        else:
-            debug("lock_createfunc (new value)")
-            if not creation_lock.acquire(wait=False):
-                raise NewValueInProgressException()
-
-        create_thread_spawned = threading.Event()
+        # see if someone created the value already
+        self.namespace.acquire_read_lock()
         try:
-            # see if someone created the value already
-            self.namespace.acquire_read_lock()
-            try:
-                has_value = self.has_value()
-                if has_value:
-                    try:
-                        value = self._Value__get_value()
-                        if not self._is_expired():
-                            return value
-                    except KeyError:
-                        # guard against un-mutexed backends raising KeyError
-                        pass
-            finally:
-                self.namespace.release_read_lock()
-
-            debug("get_value creating new value")
-            # Return the current value and spawn a thread to update it.
-            def do_update():
-                create_thread_spawned.set()
-                v = self.createfunc()
-                self.set_value(v)
-                creation_lock.release()
-            update_thread = threading.Thread(target=do_update)
-            update_thread.start()
-            create_thread_spawned.wait()
+            has_value = self.has_value()
             if has_value:
-                return value
-            else:
-                raise NewValueInProgressException()
+                try:
+                    value = self._Value__get_value()
+                    if not self._is_expired():
+                        return value
+                except KeyError:
+                    # guard against un-mutexed backends raising KeyError
+                    pass
         finally:
-            if not create_thread_spawned.isSet():
-                creation_lock.release()
-                debug("released create lock")
+            self.namespace.release_read_lock()
+
+        debug("get_value creating new value")
+        # Return the current value and spawn a thread to update it.
+        def do_update():
+            v = self.createfunc()
+            self.set_value(v)
+        self.queue.put(Update(self.key, do_update))
+        if has_value:
+            return value
+        else:
+            raise NewValueInProgressException()
+
+def update_processor(queue):
+    error('Started update processor.')
+    while True:
+        update = queue.get()
+        try:
+            debug('Running update for %s', update.update_for)
+            update.callback()
+        except Exception, e:
+            error("Exception while loading %s: %r", update.update_for, e)
+            debug(''.join(traceback.format_exception(*sys.exc_info())))
